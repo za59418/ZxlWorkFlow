01dece0cc8b355bfe5f02c7cda797db25df70a29
za59418 <za59418@51adbd81-f1f5-45e2-b070-df835a97c4a4>
2017-04-12 05:25:02 +0000

    登陆

---
+++ trunk/projects/Zxl.WebSite/ModelView/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zxl.WebSite.ModelView
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "用户名")]
+        [StringLength(10, ErrorMessage = "必须至少包含{2}个字符", MinimumLength = 3)]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "记住我？")]
+        public bool RememberMe { get; set; }
+    }
+}
