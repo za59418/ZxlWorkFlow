83ba9f75f320daffdb00e1d0f5b7dea406af5cdb
za59418 <za59418@51adbd81-f1f5-45e2-b070-df835a97c4a4>
2017-04-13 02:53:01 +0000

    未登陆时登陆跳转到登陆页

--- trunk/projects/Zxl.WebSite/App_Start/FilterConfig.cs
+++ trunk/projects/Zxl.WebSite/App_Start/FilterConfig.cs
@@ -8,6 +8,24 @@ namespace Zxl.WebSite
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new CheckLoginAttribute());
+        }
+
+        public class CheckLoginAttribute : AuthorizeAttribute
+        {
+            protected override bool AuthorizeCore(HttpContextBase httpContext)
+            {
+                bool Pass = false;
+                if (null == httpContext.Session["UserId"])
+                {
+                    Pass = false;
+                }
+                else
+                {
+                    Pass = true;
+                }
+                return Pass;
+            }
         }
     }
 }
