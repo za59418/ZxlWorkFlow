415fd4a9b7ecc6f071b1808afe5f64ffaaceb656
za59418 <za59418@51adbd81-f1f5-45e2-b070-df835a97c4a4>
2017-04-13 04:00:00 +0000

    上传材料1

--- trunk/projects/Zxl.WebSite/Controllers/WorkbenchController.cs
+++ trunk/projects/Zxl.WebSite/Controllers/WorkbenchController.cs
@@ -11,29 +11,6 @@ namespace Zxl.WebSite.Controllers
 {
     public class WorkbenchController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult Projects()
-        {
-            List<SYS_PROJECT> list = null;
-            string UserId = Session["UserId"].ToString();
-            using (ORMHandler orm = Zxl.Data.DatabaseManager.ORMHandler)
-            {
-                list = orm.Query<SYS_PROJECT>("where ID in (select ref_project_id from sys_projectworkitem where ref_user_id = " + UserId + " and state=0)");
-            }
-            foreach(SYS_PROJECT prj in list)
-            {
-                prj._parentId = prj.PID;
-            }
-
-            var jss = new System.Web.Script.Serialization.JavaScriptSerializer();
-            System.Web.HttpContext.Current.Response.Write(jss.Serialize(new { rows = list }));
-            System.Web.HttpContext.Current.Response.End();
-            return Json(new { rows = list });
-        }
 
     }
 }
