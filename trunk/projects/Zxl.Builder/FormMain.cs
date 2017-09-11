4870677b8a3e567a6bc3e026f276460e81efb521
za59418 <za59418@51adbd81-f1f5-45e2-b070-df835a97c4a4>
2017-09-11 06:13:20 +0000

    定制端

---
+++ trunk/projects/Zxl.Builder/FormMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zxl.Builder
+{
+    public partial class FormMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void metaData_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
