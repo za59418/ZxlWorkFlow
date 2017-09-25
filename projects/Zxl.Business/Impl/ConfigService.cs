394185c6fb69d4ddbd8bc9fb8abc2d440f619797
za59418 <za59418@51adbd81-f1f5-45e2-b070-df835a97c4a4>
2017-09-25 08:54:28 +0000

    配置

---
+++ projects/Zxl.Business/Impl/ConfigService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zxl.Business.Interface;
+using Zxl.Business.Model;
+using Zxl.Data;
+
+namespace Zxl.Business.Impl
+{
+    public class ConfigService : IConfigService
+    {
+        #region 配置
+        public List<SYS_SYSTEMCONFIG> Configs()
+        {
+            using (ORMHandler orm = Zxl.Data.DatabaseManager.ORMHandler)
+            {
+                return orm.Query<SYS_SYSTEMCONFIG>();
+            }
+        }
+        public int DelConfig(int ConfigId)
+        {
+            using (ORMHandler orm = Zxl.Data.DatabaseManager.ORMHandler)
+            {
+                return orm.Delete<SYS_SYSTEMCONFIG>("where ID=" + ConfigId);
+            }
+        }
+        public SYS_SYSTEMCONFIG SaveConfig(SYS_SYSTEMCONFIG config)
+        {
+            if (0 == config.ID)
+            {
+                config.ID = ValueOperator.CreatePk("SYS_SYSTEMCONFIG");
+            }
+            using (ORMHandler orm = Zxl.Data.DatabaseManager.ORMHandler)
+            {
+                orm.Save(config);
+                return config;
+            }
+        }
+        public SYS_SYSTEMCONFIG GetConfig(int ConfigId)
+        {
+            using (ORMHandler orm = Zxl.Data.DatabaseManager.ORMHandler)
+            {
+                return orm.Init<SYS_SYSTEMCONFIG>("where ID=" + ConfigId);
+            }
+        }
+        #endregion 配置
+
+    }
+}
---
+++ projects/Zxl.Business/Interface/IConfigService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zxl.Business.Model;
+
+namespace Zxl.Business.Interface
+{
+    public interface IConfigService
+    {
+        List<SYS_SYSTEMCONFIG> Configs();
+        int DelConfig(int ConfigId);
+        SYS_SYSTEMCONFIG SaveConfig(SYS_SYSTEMCONFIG config);
+        SYS_SYSTEMCONFIG GetConfig(int ConfigId);
+    }
+}
